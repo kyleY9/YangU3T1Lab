ec176ebfd42efcfc83b54c5a5787c4336bc9b2b1
KyleY9 <KyleY9@nycstudents.net>
2023-10-24 12:34:52 -0400

    Adjustments

--- src/DogRunner.java
+++ src/DogRunner.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class DogRunner {
+public class DogRunner {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Spot", 10, false);
+        Dog dog2 = new Dog("Petunia", 12, true);
+        Dog dog3 = new Dog("Lucky", 3, false);
+        Dog dog4 = new Dog("Spot", 5, true);
+        Dog dog5 = new Dog("Petunia", 12, true);
+
+        // reference variables
+        Dog dog6 = dog1;
+        Dog dog7 = dog2;
+
+        System.out.println(dog6 == dog1);
+        System.out.println(dog1 == dog7);
+    }
 }
